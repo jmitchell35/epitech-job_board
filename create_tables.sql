0e4604b5529bc9ca7af9543469a025a35a967d10
Augustin JOUAN <augustin.jouan@epitech.eu>
2025-10-07 14:22:43 +0200

    add table recruiter and company

--- create_tables.sql
+++ create_tables.sql
@@ -1,40 +1,40 @@
-DROP `job_board`;
+DROP "job_board";
 
-CREATE DATABASE `job_board`;
+CREATE DATABASE "job_board";
 
-CREATE TABLE IF NOT EXISTS `users` (
-    `user_id` CHAR(32) PRIMARY KEY,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
-    `password` VARCHAR(255),
-    `user_profile` VARCHAR(255)
+CREATE TABLE IF NOT EXISTS "users" (
+    "user_id" CHAR(32) PRIMARY KEY,
+    "email" VARCHAR(255) NOT NULL UNIQUE,
+    "password" VARCHAR(255) NOT NULL,
+    "user_profile" VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `candidate_profile` (
-    `c_profile_id` VARCHAR(32) PRIMARY KEY,
-    `first_name` VARCHAR(255),
-    `last_name` VARCHAR(255),
-    `phone` VARCHAR(255),
-    `message` TEXT,
-    `user` VARCHAR(32) REFERENCES `users` (`user_id`)
-    );
-
-/* Je me suis arrete ici <3 */
-
-CREATE TABLE IF NOT EXISTS reviews (
-    id CHAR(36) PRIMARY KEY,
-    text TEXT NOT NULL,
-    rating INT NOT NULL CHECK (rating BETWEEN 1 AND 5),
-    user_id CHAR(36) NOT NULL,
-    place_id CHAR(36) NOT NULL,
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    CONSTRAINT fk_place FOREIGN KEY (place_id) REFERENCES places(id) ON DELETE CASCADE,
-    CONSTRAINT one_review_per_owner_place UNIQUE (user_id, place_id)
+CREATE TABLE IF NOT EXISTS "candidate_profile" (
+    "c_profile_id" VARCHAR(32) PRIMARY KEY,
+    "first_name" VARCHAR(255) NOT NULL,
+    "last_name" VARCHAR(255) NOT NULL,
+    "phone" VARCHAR(255) NOT NULL,
+    "message" TEXT,
+    "user" VARCHAR(32) REFERENCES "users" ("user_id")
 );
 
-CREATE TABLE IF NOT EXISTS amenities (
-    id CHAR(36) PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE
+CREATE TABLE IF NOT EXISTS "recruiter" (
+    "recruiter_id" CHAR(32) PRIMARY KEY,
+    "company" VARCHAR (255) NOT NULL UNIQUE,
+    "application_email" VARCHAR(255) NOT NULL UNIQUE,
+    "user" VARCHAR(32) REFERENCES "users" ("user_id"),
+    "company" VARCHAR(32) REFERENCES "company" ("company_id")
 );
+
+CREATE TABLE IF NOT EXISTS "company" (
+    "company_id" CHAR(32) PRIMARY KEY,
+    "name" VARCHAR (255) NOT NULL,
+    "industry" VARCHAR(255) NOT NULL,
+    "contact_email" VARCHAR(255) NOT NULL,
+    "head_office_address" VARCHAR(255) NOT NULL
+);
+/* Je me suis arrete par la ;) */
+
 
 CREATE TABLE IF NOT EXISTS place_amenity (
     place_id CHAR(36) NOT NULL,
