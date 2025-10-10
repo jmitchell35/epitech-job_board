889c6ba5c6573264860ec18132e60ed0b4705505
Matheo Larche <matheo.larche@epitech.eu>
2025-10-10 14:35:01 +0200

    feat (endpoints) : create 2 endpoints (job add and recruiter)

--- myapp/api/v1/job_ad_endpoints.js
+++ myapp/api/v1/job_ad_endpoints.js
@@ -1,12 +1,59 @@
 // Still need the express class
 import express from 'express';
+import jobAdGateway from '../../gateways/job_ad_gateway.js';
 
 // instanciate a router object for v1 routes
 const jobAdRouter = express.Router({mergeParams: true});
 
 // use it
 jobAdRouter.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+  const promise = jobAdGateway.getAll();
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+});
+
+jobAdRouter.get('/:uuid', (req, res) => {
+  const promise = jobAdGateway.get(req.params.uuid);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+});
+
+jobAdRouter.post('/', (req, res) => {
+  const promise = jobAdGateway.post(req.body);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+});
+
+jobAdRouter.put('/:uuid', (req, res) => {
+  const promise = jobAdGateway.put(req.params.uuid, req.body);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+});
+
+jobAdRouter.delete('/:uuid', (req, res) => {
+  const promise = jobAdGateway.delete(req.params.uuid);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+});
 
 export default jobAdRouter;
--- myapp/api/v1/recruiter_endpoints.js
+++ myapp/api/v1/recruiter_endpoints.js
@@ -1,12 +1,59 @@
 // Still need the express class
 import express from 'express';
+import recruiterGateway from '../../gateways/recruiter_gateway.js';
 
 // instanciate a router object for v1 routes
 const recruiterRouter = express.Router({mergeParams: true});
 
 // use it
 recruiterRouter.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+  const promise = recruiterGateway.getAll();
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+});
+
+recruiterRouter.get('/:uuid', (req, res) => {
+  const promise = recruiterGateway.get(req.params.uuid);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+});
+
+recruiterRouter.post('/', (req, res) => {
+  const promise = recruiterGateway.post(req.body);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+});
+
+recruiterRouter.put('/:uuid', (req, res) => {
+  const promise = recruiterGateway.put(req.params.uuid, req.body);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+});
+
+recruiterRouter.delete('/:uuid', (req, res) => {
+  const promise = recruiterGateway.delete(req.params.uuid);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+});
 
 export default recruiterRouter;
