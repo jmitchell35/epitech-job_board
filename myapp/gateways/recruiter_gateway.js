0f9b188d3a67bf6a48dc80a81c309d0d86a40ed3
Matheo Larche <matheo.larche@epitech.eu>
2025-10-10 12:24:43 +0200

    feat (gateway) : create 2 gateway (recruiter and advertissement)

---
+++ myapp/gateways/recruiter_gateway.js
@@ -0,0 +1,48 @@
+import prisma from '../persistence/prisma.js';
+
+class RecruiterGateway {
+  async getAll() {
+    let data = await prisma.recruiter.findMany();
+    console.log(data);
+    return data;
+  }
+
+  async get(uuid) {
+    let data = await prisma.recruiter.findUnique({
+      where: {
+        id: uuid
+      }
+    });
+    console.log(data);
+    return data;
+  }
+
+  async create(jsonData) {
+    let createdData = await prisma.recruiter.create(jsonData)
+    console.log(createdData);
+    return createdData;
+  }
+
+  async update(uuid, jsonData) {
+    let updatedData = await prisma.recruiter.update({
+      where: {
+        id: uuid
+      },
+      data: jsonData,
+    });
+    console.log(updatedData);
+    return updatedData;
+  }
+
+  async delete(uuid) {
+    let deletedData = await prisma.recruiter.delete({
+      where: {
+        id: uuid,
+      }
+    });
+    console.log(deletedData);
+    return deletedData;
+  }
+}
+
+export default new RecruiterGateway();
