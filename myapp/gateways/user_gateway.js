5bbf6c179e6382addcee40ece28112f6f75770e7
jmitchell35 <julian.m.mitchell@gmail.com>
2025-10-10 11:42:33 +0200

    user_gateway

--- myapp/gateways/user_gateway.js
+++ myapp/gateways/user_gateway.js
@@ -1,7 +1,46 @@
 import prisma from '../persistence/prisma.js';
 
 class UserGateway {
-  getAll() {
-    return prisma.user.findMany();
+  async getAll() {
+    let data = await prisma.user.findMany();
+    console.log(data);
+    return data;
+  }
+
+  async get(uuid) {
+    let data = await prisma.user.findUnique({
+      where: {
+        id: uuid
+      }
+    });
+    console.log(data);
+    return data;
+  }
+
+  async getByEmail(email) {
+    let data = await prisma.user.findUnique({
+      where: {
+        email
+      }
+    });
+    console.log(data);
+    return data;
+  }
+
+  async create(jsonData) {
+    let createdData = await prisma.user.create(jsonData)
+    console.log(createdData);
+    return createdData;
+  }
+
+  async update(uuid, jsonData) {
+    let updatedData = await prisma.user.update({
+      where: {
+        id: uuid
+      },
+      data: jsonData,
+    });
+    console.log(updatedData);
+    return updatedData;
   }
 }
