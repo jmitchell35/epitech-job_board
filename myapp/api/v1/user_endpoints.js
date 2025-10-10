c6bbf28304febf1f324916455a631d938e6ea4d2
jmitchell35 <julian.m.mitchell@gmail.com>
2025-10-10 14:22:11 +0200

    Pushing user_endpoint

--- myapp/api/v1/user_endpoints.js
+++ myapp/api/v1/user_endpoints.js
@@ -1,16 +1,61 @@
 // Still need the express class
 import express from 'express';
+import userGateway from '../../gateways/company_gateway.js';
+
 
 // instanciate a router object for v1 routes
-const userRouter = express.Router();
+const userRouter = express.Router({mergeParams: true});
+
 
 // use it
 userRouter.get('/', (req, res) => {
-  res.send('Hello World!');
+  const promise = userGateway.getAll();
+    promise.then((data) => {
+      res.send(data);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 })
 
 userRouter.get('/:uuid', (req, res) => {
-  res.send(`Hello ${req.params.uuid}!`);
+  const promise = userGateway.get(req.params.uuid);
+    promise.then((data) => {
+      res.send(data);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+})
+
+userRouter.post('/', (req, res) => {
+  const promise = userGateway.create(req.body);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+})
+
+userRouter.put('/:uuid', (req, res) => {
+  const promise = userGateway.update(uuid, req.body);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+})
+
+userRouter.delete('/:uuid', (req, res) => {
+  const promise = userGateway.delete(uuid);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
 })
 
 export default userRouter;
