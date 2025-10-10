9aeb4f1f58d3b5f23ea95116022a9c877aa3b37f
jmitchell35 <julian.m.mitchell@gmail.com>
2025-10-10 12:31:55 +0200

    pushing company endpoints

--- myapp/api/v1/company_endpoints.js
+++ myapp/api/v1/company_endpoints.js
@@ -27,7 +27,27 @@ companyRouter.get('/:uuid', (req, res) => {
 })
 
 companyRouter.post('/', (req, res) => {
-  const promise = companyGateway.post(req.body);
+  const promise = companyGateway.create(req.body);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+})
+
+companyRouter.put('/:uuid', (req, res) => {
+  const promise = companyGateway.update(uuid, req.body);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+})
+
+companyRouter.delete('/:uuid', (req, res) => {
+  const promise = companyGateway.delete(uuid);
   promise.then((data) => {
     res.send(data);
   })
