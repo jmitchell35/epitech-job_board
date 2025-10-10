4dc67791a676cf47ca8cb84e45b4e9f34d7e75d0
Augustin JOUAN <augustin.jouan@epitech.eu>
2025-10-10 14:41:01 +0200

    feat(candidate_endpoint &  application_endpoint): 2 new endpoint

--- myapp/api/v1/candidate_endpoints.js
+++ myapp/api/v1/candidate_endpoints.js
@@ -1,12 +1,59 @@
 // Still need the express class
 import express from 'express';
+import candidateGateway from '../../gateways/candidate_gateway.js';
 
 // instanciate a router object for v1 routes
 const candidateRouter = express.Router({mergeParams: true});
 
 // use it
 candidateRouter.get('/', (req, res) => {
-  res.send('Hello World!');
+  const promise = candidateGateway.getAll();
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+})
+
+candidateRouter.get('/:uuid', (req, res) => {
+  const promise = candidateGateway.get(req.params.uuid);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+})
+
+candidateRouter.post('/', (req, res) => {
+  const promise = candidateGateway.post(req.body);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+})
+
+candidateRouter.put('/:uuid', (req, res) => {
+  const promise = candidateGateway.put(req.body);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+})
+
+candidateRouter.delete('/:uuid', (req, res) => {
+  const promise = candidateGateway.delete(req.body);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
 })
 
 export default candidateRouter;
--- myapp/api/v1/job_application_endpoints.js
+++ myapp/api/v1/job_application_endpoints.js
@@ -1,12 +1,59 @@
 // Still need the express class
 import express from 'express';
+import applicationGateway from '../../gateways/application_gateway.js';
 
 // instanciate a router object for v1 routes
-const jobApplicationRouter = express.Router({mergeParams: true});
+const applicationRouter = express.Router({mergeParams: true});
 
 // use it
-jobApplicationRouter.get('/', (req, res) => {
-  res.send('Hello World!');
+applicationRouter.get('/', (req, res) => {
+  const promise = applicationGateway.getAll();
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
 })
 
-export default jobApplicationRouter;
+applicationRouter.get('/:uuid', (req, res) => {
+  const promise = applicationGateway.get(req.params.uuid);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+})
+
+applicationRouter.post('/', (req, res) => {
+  const promise = applicationGateway.post(req.body);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+})
+
+applicationRouter.put('/:uuid', (req, res) => {
+  const promise = applicationGateway.put(req.body);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+})
+
+applicationRouter.delete('/:uuid', (req, res) => {
+  const promise = applicationGateway.delete(req.body);
+  promise.then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+})
+
+export default applicationRouter;
