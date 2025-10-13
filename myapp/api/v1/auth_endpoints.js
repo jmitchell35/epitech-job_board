50782086648cf1a1bbd187a0af19695491f56b2a
jmitchell35 <julian.m.mitchell@gmail.com>
2025-10-13 15:21:55 +0200

    feat(auth): adding logout, refining variable names

--- myapp/api/v1/auth_endpoints.js
+++ myapp/api/v1/auth_endpoints.js
@@ -9,30 +9,33 @@ const authRouter = express.Router({mergeParams: true});
 
 // use it
 authRouter.post('/login', (req, res) => {
-  console.log(req.body);
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
-  const promise = userGateway.getByEmail(username);
+  const promise = userGateway.getByEmail(email);
   promise.then((user) => {
     console.log(user);
 
     if (user !== null && password === user.password) {
-      console.log('User found and password verified');
       // Encodage du JWT via la variable d'environnement JWT_SECRET
-      const jwtToken = jwt.sign({ username }, process.env.JWT_SECRET, {
+      const jwtToken = jwt.sign({ email, profile }, process.env.JWT_SECRET, {
         expiresIn: "1d",
       });
 
       res.cookie("jwtToken", jwtToken, { httpOnly: true, secure: true });
       res.json(jwtToken);
     } else {
-		  res.status(401).json({ message: "Wrong credentials." });
+		  res.status(401).json({ message: "Identifiants incorrects." });
 	  }
   })
   .catch((error) => {
     console.log(error);
     res.send(error);
   });
-})
+});
+
+authRouter.get('/logout', (req, res) => {
+  res.clearCookie("jwtToken");
+	res.redirect('/');
+});
 
 export default authRouter;
